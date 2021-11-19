1d6afaba14a79a99b3df3ccd5f72d4e5c2580848
George Zhou <georgezhou89@hotmail.com>
2021-11-18 19:36:56 -0500

    Added Menu

--- Assets/Scripts/ObjectPool.cs
+++ Assets/Scripts/ObjectPool.cs
@@ -6,13 +6,10 @@ public class ObjectPool : MonoBehaviour
 {
 
     public static ObjectPool SharedInstance;
-    public GameObject o1;
-    public GameObject o2;
     public List<GameObject> objectToPool;
-    //public GameObject objectToPool2;
-    //public GameObject objectToPool3;
     public int amountToPool;
     public List<GameObject> pooledObjects;
+    public int poolCounter = -1;
 
     private void Awake()
     {
@@ -22,48 +19,47 @@ public class ObjectPool : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        objectToPool.Add(o1);
-        objectToPool.Add(o2);
-        //Debug.Log(objectToPool);
-        
-        BuildPool(0,2);
+        BuildPool();
         //Debug.Log(pooledObjects.Count);
     }
 
-    private void BuildPool(int index=0)
+    // Pool list of random objects
+    private void BuildPool()
     {
         pooledObjects = new List<GameObject>();
         GameObject tmp;
         for (int i = 0; i < amountToPool; i++)
         {
-            tmp = Instantiate(objectToPool[0]);
+            tmp = Instantiate(objectToPool[Random.Range(0, objectToPool.Count)]);
             tmp.SetActive(false);
             pooledObjects.Add(tmp);
         }
     }
 
-    private void BuildPool(int min, int max)
-    {
-        pooledObjects = new List<GameObject>();
-        GameObject tmp;
-        for (int i = 0; i < amountToPool; i++)
-        {
-            tmp = Instantiate(objectToPool[Random.Range(min,max)]);
-            tmp.SetActive(false);
-            pooledObjects.Add(tmp);
-        }
-    }
+    //private void BuildPool(int min, int max)
+    //{
+    //    pooledObjects = new List<GameObject>();
+    //    GameObject tmp;
+    //    for (int i = 0; i < amountToPool; i++)
+    //    {
+    //        tmp = Instantiate(objectToPool[Random.Range(min,max)]);
+    //        tmp.SetActive(false);
+    //        pooledObjects.Add(tmp);
+    //    }
+    //}
 
     // Return pooled object if it is not in the active hierarchy
     public GameObject GetPooledObject()
     {
-        for (int i = 0; i < amountToPool; i++)
+        poolCounter++;
+        if (poolCounter>= amountToPool)
         {
-            if (!pooledObjects[i].activeInHierarchy)
-            {
-                return pooledObjects[i];
-            }
+            poolCounter = -1;
         }
-        return null;
+        if (!pooledObjects[poolCounter].activeInHierarchy&&poolCounter<amountToPool)
+        {
+            return pooledObjects[poolCounter];
+        }
+        return GetPooledObject();   
     }
 }
